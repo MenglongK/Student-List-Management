21365b36a85424b2b54bc3cd07a3c7abefa7069e
MenglongK <menglongkeo07@gmail.com>
2025-08-07 17:05:00 +0700

    Third initialize

--- src/index.js
+++ src/index.js
@@ -9,14 +9,19 @@ const staticArray = [
   },
 ];
 
-const displayNames = staticArray.map((user, index) => {
-  return `<div
+let dynamicArray = JSON.parse(sessionStorage.getItem("dynamicUsers")) || [];
+const combineArray = [...staticArray, ...dynamicArray];
+const allStudent = document.getElementById("totalStudent");
+allStudent.innerHTML = `<h3>Total Student: <span class="text-purple-700">${combineArray.length}</span></h3>`;
+function displayCards() {
+  const cards = combineArray.map((user, index, totalStudent) => {
+    return `<div
     class="p-4 md:p-5 border-l-4 border-blue-500 rounded-lg shadow-xl hover:scale-102 duration-500 bg-[#EEF7FF]"
     >
     <div class="flex justify-between items-center gap-x-3">
     <div class="grow">
     <h3
-    class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200"
+    class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200 hover:text-blue-500"
     >
     ${index + 1}. ${user.name} 
     </h3>
@@ -26,7 +31,26 @@ const displayNames = staticArray.map((user, index) => {
     </div>
     </div>
     </div>`;
-});
-const staticData = document.getElementById("staticData");
-console.log(displayNames);
-staticData.innerHTML = displayNames.join("");
+  });
+  document.getElementById("combineData").innerHTML = cards.join("");
+}
+
+displayCards();
+function addUser() {
+  const name = document.getElementById("nameInput").value;
+  const grade = document.getElementById("gradeInput").value;
+  if (name && grade) {
+    dynamicArray.push({ name, grade });
+    sessionStorage.setItem("dynamicUsers", JSON.stringify(dynamicArray));
+    document.getElementById("nameInput").value = "";
+    document.getElementById("gradeInput").value = "";
+    displayCards();
+  } else {
+    alert("Please complete all field!!!");
+  }
+}
+function resetData() {
+  dynamicArray = [];
+  sessionStorage.removeItem("dynamicUsers");
+  displayCards();
+}
