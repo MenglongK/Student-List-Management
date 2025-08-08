986daba595bc147f7f8a30136e9de52fa1925089
MenglongK <menglongkeo07@gmail.com>
2025-08-08 16:20:03 +0700

    Update javascript

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const staticArray = [
   {
     name: "Keo Menglong",
-    grade: "A",
+    grade: "B",
   },
   {
     name: "Mao Piseth",
@@ -12,20 +12,20 @@ const staticArray = [
 let dynamicArray = JSON.parse(sessionStorage.getItem("dynamicUsers")) || [];
 const combineArray = [...staticArray, ...dynamicArray];
 const allStudent = document.getElementById("totalStudent");
-allStudent.innerHTML = `<h3>Total Student: <span class="text-purple-700">${combineArray.length}</span></h3>`;
+allStudent.innerHTML = `<h3 class="font-semibold">Total Student: <span class="text-purple-700">${combineArray.length}</span></h3>`;
 function displayCards() {
-  const cards = combineArray.map((user, index, totalStudent) => {
+  const cards = combineArray.map((user, index) => {
     return `<div
-    class="p-4 md:p-5 border-l-4 border-blue-500 rounded-lg shadow-xl hover:scale-102 duration-500 bg-[#EEF7FF]"
+    class="p-4 md:p-5 border-l-4 border-blue-500 rounded-lg shadow-xl md:hover:scale-102 duration-500 bg-[#EEF7FF]"
     >
     <div class="flex justify-between items-center gap-x-3">
     <div class="grow">
     <h3
-    class="group-hover:text-blue-600 font-semibold text-gray-800 dark:group-hover:text-neutral-400 dark:text-neutral-200 hover:text-blue-500"
+    class="group-hover:text-blue-600 font-semibold text-gray-800 hover:text-blue-500"
     >
     ${index + 1}. ${user.name} 
     </h3>
-    <p class="text-sm text-gray-500 dark:text-neutral-500">
+    <p class="text-sm text-gray-500">
     Grade: <span class="text-blue-500 font-bold uppercase">${user.grade}</span>
     </p>
     </div>
@@ -54,3 +54,15 @@ function resetData() {
   sessionStorage.removeItem("dynamicUsers");
   displayCards();
 }
+
+const filterInput = document.getElementById("filterInput");
+const inputBtn = document
+  .getElementById("inputBtn")
+  .addEventListener("click", () => {
+    const finalInput = filterInput.value;
+    combineArray.filter((arg) => {
+      return arg.grade == finalInput;
+    });
+    
+  });
+
